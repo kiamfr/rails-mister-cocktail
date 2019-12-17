f735205294c6257c533e98a8e47215a3e513bcfa
Maik Froebisch <maik.froebisch@gmail.com>
2019-12-17 19:29:11 +0100

    added delete function for dose

---
+++ db/migrate/20191214150943_add_column_picture_url_to_cocktails.rb
@@ -0,0 +1,5 @@
+class AddColumnPictureUrlToCocktails < ActiveRecord::Migration[5.2]
+  def change
+    add_column :cocktails, :picture_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+file_path = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json_file = open(file_path).read
+ingredient_list = JSON.parse(json_file)
+Ingredient.destroy_all
+5.times do
+  ingredient_item = ingredient_list["drinks"].sample["strIngredient1"]
+  ingredient = Ingredient.new(name: ingredient_item)
+  ingredient.save
+end
+
+#Ingredient.create(ingredient_list)
