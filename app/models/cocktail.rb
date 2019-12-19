042f3a8282c1560ff0467b4ebf303981318c1608
Maik Froebisch <maik.froebisch@gmail.com>
2019-12-19 21:26:26 +0100

    made photo mandatory

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
   has_many :doses, :dependent => :destroy
   has_many :ingredients, through: :doses, :dependent => :destroy
   mount_uploader :photo, PhotoUploader
