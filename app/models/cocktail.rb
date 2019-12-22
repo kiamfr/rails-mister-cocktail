8bec6c44f2a2e904c614767c0c35dd5ec6da0825
Maik Froebisch <maik.froebisch@gmail.com>
2019-12-22 20:19:59 +0100

    added default picture for uploads & changed ingredient management

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,5 @@
 class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :photo, presence: true
   has_many :doses, :dependent => :destroy
   has_many :ingredients, through: :doses, :dependent => :destroy
   mount_uploader :photo, PhotoUploader
