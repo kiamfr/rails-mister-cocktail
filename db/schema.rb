5d6f2bd101f9ffc19aad5ed4b407b642d4acc22b
Maik Froebisch <maik.froebisch@gmail.com>
2019-12-27 09:42:22 +0100

    creates Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_24_074107) do
+ActiveRecord::Schema.define(version: 2019_12_27_084019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_12_24_074107) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
